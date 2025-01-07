97919da9eef7a5519e794e8e2651675453f2728d
ankit_049 <ankitrajputji00@gmail.com>
2025-01-07 13:58:06 +0530

    AIML Added

--- FIrst.java
+++ FIrst.java
@@ -1,5 +1,6 @@
 class First{
     public static void main(String[] args) {
-        System.out.println("Welcome IN Fisrt Program");
+        System.out.println("Welcome IN Fisrt Program"); 
+        System.out.println("Welcome note for AIML Branch");
     }
 }
